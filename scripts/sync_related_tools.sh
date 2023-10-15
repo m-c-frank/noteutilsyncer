aace0133d22364ee0ff68ad5054dc2b5f18a0f18
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-15 20:21:48 +0200

    added repo loop in sh

--- scripts/sync_related_tools.sh
+++ scripts/sync_related_tools.sh
@@ -8,14 +8,20 @@ sed -n '/<!--START_TAG-->/, /<!--END_TAG-->/p' README.md > current_related_tools
 
 # Compare the Gist content with the current section
 if ! diff gist_related_tools.txt current_related_tools.txt; then
-  # Update the README
-  sed -i '/<!--START_TAG-->/, /<!--END_TAG-->/c\<!--START_TAG-->\n'$(cat gist_related_tools.txt)'\n<!--END_TAG-->' README.md
-  git config --global user.name 'GitHub Action'
-  git config --global user.email 'action@github.com'
-  git add README.md
-  git commit -m 'Update Related Tools section from Gist'
-  git push
-  
-  # Extract repo URLs and update them
-  python scripts/utils.py
+    # Update the README
+    sed -i '/<!--START_TAG-->/, /<!--END_TAG-->/c\<!--START_TAG-->\n'$(cat gist_related_tools.txt)'\n<!--END_TAG-->' README.md
+    git config --global user.name 'GitHub Action'
+    git config --global user.email 'action@github.com'
+    git add README.md
+    git commit -m 'Update Related Tools section from Gist'
+    git push
+
+    # Update related repositories' READMEs
+    for repo in $(cat gist_related_tools.txt); do
+    python scripts/utils.py update_repo_readme $repo
+    done
+
+
+    # Extract repo URLs and update them
+    python scripts/utils.py
 fi
