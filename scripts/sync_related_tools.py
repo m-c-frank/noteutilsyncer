6d9ae5ed72b9582a1919177ee9caf002f894b9b2
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-15 20:42:02 +0200

    fixing

--- scripts/sync_related_tools.py
+++ scripts/sync_related_tools.py
@@ -1,30 +1,36 @@
 import os
 import subprocess
-from utils import check_and_update_readme, fetch_gist_content, split_gist_into_repos
+import utils
 
 def main():
-    # Check and update the main README if necessary
-    if check_and_update_readme():
+    # Check and update the main repo's README
+    if utils.check_and_update_readme():
+        # Commit and push changes to the main repo
         subprocess.run(["git", "config", "--global", "user.name", "GitHub Action"])
         subprocess.run(["git", "config", "--global", "user.email", "action@github.com"])
         subprocess.run(["git", "add", "README.md"])
-        subprocess.run(["git", "commit", "-m", 'Update Related Tools section from Gist'])
-        subprocess.run(["git", "push", "origin", "main", "--force", "--quiet", "https://${{GH_PAT}}@github.com/m-c-frank/noteutilsyncer.git"])
+        subprocess.run(["git", "commit", "-m", "Update Related Tools section from Gist"])
+        subprocess.run(["git", "push", "origin", "main", "--force", "--quiet", f"https://{os.environ['GH_PAT']}@github.com/m-c-frank/noteutilsyncer.git"])
 
-    # Fetch the list of repo names from the Gist
-    repo_names = split_gist_into_repos(fetch_gist_content())
+        # Fetch the updated list of repos from the gist
+        repo_names = utils.split_gist_into_repos(utils.fetch_gist_content())
 
-    # Loop through each repo and update its README
-    for repo in repo_names:
-        if repo != "noteutilsyncer":
+        for repo in repo_names:
+            # Clone the repo
             subprocess.run(["git", "clone", f"https://github.com/m-c-frank/{repo}.git"])
+            
+            # Change directory to the cloned repo
             os.chdir(repo)
 
-            if check_and_update_readme():
-                subprocess.run(["git", "add", "README.md"])
-                subprocess.run(["git", "commit", "-m", 'Update Related Tools section from central Gist'])
-                subprocess.run(["git", "push", "origin", "main", "--force", "--quiet", f"https://${{GH_PAT}}@github.com/m-c-frank/{repo}.git"])
+            # Update the README
+            utils.check_and_update_readme()
 
+            # Commit and push changes
+            subprocess.run(["git", "add", "README.md"])
+            subprocess.run(["git", "commit", "-m", "Update Related Tools section from Gist"])
+            subprocess.run(["git", "push", "origin", "main", "--force", "--quiet", f"https://{os.environ['GH_PAT']}@github.com/m-c-frank/{repo}.git"])
+
+            # Change directory back to the main directory
             os.chdir("..")
 
 if __name__ == "__main__":
