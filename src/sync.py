2d27cb53966ada965984757c060d3a50e52a4386
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-15 20:06:30 +0200

    added tests and refactored a lot

--- src/sync.py
+++ src/sync.py
@@ -1,14 +1,26 @@
 import os
 import re
 import subprocess
+import requests
+import json
 
-def extract_related_tools_section(filename):
+# Constants
+START_TAG = "<!--START_TAG-->"
+END_TAG = "<!--END_TAG-->"
+GH_PAT = os.environ.get('GH_PAT')
+
+
+def read_file(filename):
     with open(filename, 'r') as file:
-        content = file.read()
+        return file.read()
 
-    start_tag = "<!--START_TAG-->"
-    end_tag = "<!--END_TAG-->"
 
+def write_file(filename, content):
+    with open(filename, 'w') as file:
+        file.write(content)
+
+
+def extract_section(content, start_tag=START_TAG, end_tag=END_TAG):
     start_index = content.find(start_tag)
     end_index = content.find(end_tag)
 
@@ -17,40 +29,81 @@ def extract_related_tools_section(filename):
 
     return content[start_index + len(start_tag):end_index].strip()
 
-def update_repository(repo_url, related_tools_section):
+
+def replace_section(content, start_tag, end_tag, new_section):
+    start_index = content.find(start_tag)
+    end_index = content.find(end_tag)
+
+    if start_index == -1 or end_index == -1:
+        raise ValueError(f"Missing section between {start_tag} and {end_tag}")
+
+    # Extract the content before the start tag, the new section, and the content after the end tag
+    return content[:start_index] + new_section + content[end_index + len(end_tag):]
+
+
+
+def git_config():
+    subprocess.run(['git', 'config', '--global', 'user.name', 'GitHub Action'])
+    subprocess.run(['git', 'config', '--global', 'user.email', 'action@github.com'])
+
+
+def clone_and_update_repo(repo_url, new_section):
     repo_name = repo_url.split('/')[-1]
     branch_name = "update-related-tools"
     
     subprocess.run(['git', 'clone', f'https://github.com/{repo_url}.git'])
     subprocess.run(['git', '-C', repo_name, 'checkout', '-b', branch_name])
     
-    with open(f'{repo_name}/README.md', 'r') as file:
-        content = file.read()
-    pattern = r"<!--START_TAG-->.*?<!--END_TAG-->"
-    replacement = f"<!--START_TAG-->\n{related_tools_section}\n<!--END_TAG-->"
-    new_content = re.sub(pattern, replacement, content, flags=re.DOTALL)
-    with open(f'{repo_name}/README.md', 'w') as file:
-        file.write(new_content)
+    content = read_file(f'{repo_name}/README.md')
+    updated_content = replace_section(content, "<!--START_TAG-->", "<!--END_TAG-->", new_section)
+    write_file(f'{repo_name}/README.md', updated_content)
     
     subprocess.run(['git', '-C', repo_name, 'add', 'README.md'])
     subprocess.run(['git', '-C', repo_name, 'commit', '-m', 'Update Related Tools section'])
     subprocess.run(['git', '-C', repo_name, 'push', 'origin', branch_name])
 
-if __name__ == "__main__":
-    related_tools_section = extract_related_tools_section('README.md')
-    
+
+def create_pull_request(repo_url, branch_name, token=GH_PAT):
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    data = {
+        'title': 'Update Related Tools section',
+        'body': 'Automated update of the Related Tools section.',
+        'head': branch_name,
+        'base': 'main'
+    }
+    response = requests.post(f'https://api.github.com/repos/{repo_url}/pulls', headers=headers, data=json.dumps(data))
+    response.raise_for_status()
+
+
+def extract_repo_urls_from_directory(directory='READMEs'):
     repositories = []
-    if os.path.exists('READMEs'):
-        for filename in os.listdir('READMEs'):
-            if filename.endswith('.md'):
-                with open(f'READMEs/{filename}', 'r') as file:
-                    match = re.search(r'https://github.com/([a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+)', file.read())
-                    if match:
-                        repositories.append(match.group(1))
+    for filename in os.listdir(directory):
+        if filename.endswith('.md'):
+            content = read_file(f'{directory}/{filename}')
+            match = re.search(r'https://github.com/[a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+', content)
+            if match:
+                repositories.append(match.group(0).split("https://github.com/")[1])
+    return repositories
+
+
+if __name__ == "__main__":
+    if not GH_PAT:
+        raise ValueError("Missing GitHub PAT token!")
+
+    content = read_file('README.md')
+    related_tools_section = extract_section(content)
+    
+    repositories = extract_repo_urls_from_directory()
+
+    git_config()
 
     for repo in repositories:
         try:
-            update_repository(repo, related_tools_section)
+            clone_and_update_repo(repo, related_tools_section)
+            create_pull_request(repo, "update-related-tools")
             print(f"Updated {repo} successfully!")
         except Exception as e:
             print(f"Failed to update {repo}. Reason: {e}")
--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,10 +1,9 @@
 import pytest
-from src.sync import extract_related_tools_section, update_repository, create_pull_request, extract_repo_url_from_readme
-import os
-import requests
-import subprocess
+from src.sync import (read_file, write_file, extract_section, replace_section,
+                      git_config, clone_and_update_repo, create_pull_request,
+                      extract_repo_urls_from_directory)
 
-# Adjusted mock content for README.md
+# Sample content for mock README.md
 MOCK_CONTENT = """
 # Project Title
 
@@ -13,76 +12,72 @@ MOCK_CONTENT = """
 - Feature B
 
 ## Related Tools
-
+<!--START_TAG-->
 - Tool A (https://github.com/user/toolA)
 - Tool B (https://github.com/user/toolB)
+<!--END_TAG-->
 
 ## License
 MIT License
 """
 
-def test_extract_related_tools_section():
-    with open("mock_readme.md", "w") as f:
-        f.write(MOCK_CONTENT)
+def test_read_file(mocker):
+    mocker.patch('builtins.open', mocker.mock_open(read_data=MOCK_CONTENT))
+    content = read_file("mock_readme.md")
+    assert "Project Title" in content
 
-    section = extract_related_tools_section("mock_readme.md")
-    assert section == "## Related Tools\n\n- Tool A (https://github.com/user/toolA)\n- Tool B (https://github.com/user/toolB)"
-    os.remove("mock_readme.md")
+def test_write_file(mocker):
+    mock_open = mocker.patch('builtins.open', mocker.mock_open())
+    write_file("mock_readme.md", MOCK_CONTENT)
+    mock_open.assert_called_once_with("mock_readme.md", "w")
 
-def test_extract_repo_url_from_readme():
-    with open("mock_readme.md", "w") as f:
-        f.write(MOCK_CONTENT)
+def test_extract_section():
+    section = extract_section(MOCK_CONTENT, "<!--START_TAG-->", "<!--END_TAG-->")
+    assert "Tool A" in section
+    assert "Tool B" in section
 
-    repo_url = extract_repo_url_from_readme("mock_readme.md")
-    assert repo_url == "user/toolA"
-    os.remove("mock_readme.md")
+def test_replace_section():
+    new_section = """
+<!--START_TAG-->
+- Tool C (https://github.com/user/toolC)
+<!--END_TAG-->
+"""
+    updated_content = replace_section(MOCK_CONTENT, "<!--START_TAG-->", "<!--END_TAG-->", new_section)
+    assert "Tool C" in updated_content
+    assert "Tool A" not in updated_content
+    assert "Tool B" not in updated_content
 
-@pytest.mark.parametrize("content, expected_url", [
-    ("""
-    ## Related Tools
-    - Tool A (https://github.com/user/toolA)
-    """, "user/toolA"),
-    ("""
-    ## Related Tools
-    - Tool B (https://github.com/user/toolB)
-    """, "user/toolB")
-])
-def test_extract_repo_url_from_readme_parametrized(content, expected_url):
-    with open("mock_readme.md", "w") as f:
-        f.write(content)
 
-    repo_url = extract_repo_url_from_readme("mock_readme.md")
-    assert repo_url == expected_url
-    os.remove("mock_readme.md")
+def test_git_config(mocker):
+    mock_subprocess = mocker.patch('subprocess.run')
+    git_config()
+    assert mock_subprocess.call_count == 2
 
+def test_clone_and_update_repo(mocker):
+    mock_subprocess = mocker.patch('subprocess.run')
+    mock_read_file = mocker.patch('src.sync.read_file', return_value=MOCK_CONTENT)
+    mock_write_file = mocker.patch('src.sync.write_file')
+    
+    clone_and_update_repo("user/repo", "## Related Tools\n- Tool A\n- Tool B")
+    
+    assert mock_subprocess.call_count == 5
+    mock_read_file.assert_called_once()
+    mock_write_file.assert_called_once()
 
 def test_create_pull_request(mocker):
-    mock_post = mocker.patch('requests.post')
+    mock_requests = mocker.patch('requests.post')
     mock_response = mocker.MagicMock()
     mock_response.status_code = 201
-    mock_post.return_value = mock_response
+    mock_requests.return_value = mock_response
     
     create_pull_request("user/repo", "branch_name", "mock_token")
+    mock_requests.assert_called_once()
+
+def test_extract_repo_urls_from_directory(mocker):
+    mock_os = mocker.patch('os.listdir', return_value=['mock_readme1.md', 'mock_readme2.md'])
+    mock_read_file = mocker.patch('src.sync.read_file', return_value=MOCK_CONTENT)
     
-    # Adjusted assertion
-    mock_post.assert_called_once()
-
-def test_update_repository(mocker):
-    mocker.patch('subprocess.run')
-    mock_response = mocker.MagicMock()
-    mock_response.status_code = 201
-    mocker.patch('requests.post', return_value=mock_response)
-
-    os.environ['GH_PAT'] = 'mock_token'
-    if not os.path.exists("repo"):
-        os.mkdir("repo")
-    with open("repo/README.md", "w") as f:
-        f.write(MOCK_CONTENT)
-    update_repository("user/repo", "## Related Tools\n- Tool A\n- Tool B")
-    os.remove("repo/README.md")
-
-    # Assert that subprocess.run was called (indicating git operations were performed)
-    assert subprocess.run.call_count > 0
-
-    # Cleanup
-    del os.environ['GH_PAT']
+    repo_urls = extract_repo_urls_from_directory('READMEs')
+    
+    assert len(repo_urls) == 2
+    assert "user/toolA" in repo_urls
