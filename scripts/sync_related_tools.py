7580a0c36ead6c6332806cdcc6dc7c4ee29577a6
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-15 20:38:50 +0200

    fixing cd

--- scripts/sync_related_tools.py
+++ scripts/sync_related_tools.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from utils import check_and_update_readme, fetch_gist_content, split_gist_into_repos
 
@@ -8,7 +9,7 @@ def main():
         subprocess.run(["git", "config", "--global", "user.email", "action@github.com"])
         subprocess.run(["git", "add", "README.md"])
         subprocess.run(["git", "commit", "-m", 'Update Related Tools section from Gist'])
-        subprocess.run(["git", "push"])
+        subprocess.run(["git", "push", "origin", "main", "--force", "--quiet", "https://${{GH_PAT}}@github.com/m-c-frank/noteutilsyncer.git"])
 
     # Fetch the list of repo names from the Gist
     repo_names = split_gist_into_repos(fetch_gist_content())
@@ -17,15 +18,14 @@ def main():
     for repo in repo_names:
         if repo != "noteutilsyncer":
             subprocess.run(["git", "clone", f"https://github.com/m-c-frank/{repo}.git"])
-            subprocess.check_call(["cd", repo])
+            os.chdir(repo)
 
             if check_and_update_readme():
                 subprocess.run(["git", "add", "README.md"])
                 subprocess.run(["git", "commit", "-m", 'Update Related Tools section from central Gist'])
-                subprocess.run(["git", "push"])
+                subprocess.run(["git", "push", "origin", "main", "--force", "--quiet", f"https://${{GH_PAT}}@github.com/m-c-frank/{repo}.git"])
 
-            subprocess.check_call(["cd", ".."])
-            subprocess.run(["rm", "-rf", repo])
+            os.chdir("..")
 
 if __name__ == "__main__":
     main()
