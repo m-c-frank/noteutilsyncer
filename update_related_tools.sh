407a7f302a0e176b24b13f951d2fa1806f41bb68
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-15 18:45:52 +0200

    fixed gist url

--- update_related_tools.sh
+++ update_related_tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GIST_CONTENT=$(curl -s https://gist.githubusercontent.com/yourusername/gistid/raw/)
+GIST_CONTENT=$(curl -s https://gist.githubusercontent.com/m-c-frank/5b7a099c0998e3030888125370b26195/raw)
 
 # Use sed to replace the "Related Tools" section in README.md with the fetched Gist content
 sed -i '/^## Related Tools$/,/^##/c\'"$GIST_CONTENT" README.md
