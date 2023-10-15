95a9a73599db2fb9f47279b6f5491792b404a333
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-15 19:52:39 +0200

    changed tags in sh

--- update_related_tools.sh
+++ update_related_tools.sh
@@ -3,8 +3,8 @@
 # Fetch the Gist content and save it to a temporary file
 curl -s https://gist.githubusercontent.com/m-c-frank/5b7a099c0998e3030888125370b26195/raw/ > temp_gist_content.txt
 
-# Use sed to replace the "Related Tools" section in README.md with the content from the temporary file
-sed -i '/^## Related Tools$/,/^##/!b; /^## Related Tools$/!d; r temp_gist_content.txt' README.md
+# Use sed to replace the content between the tags in README.md with the content from the temporary file
+sed -i '/<!--START_TAG-->/, /<!--END_TAG-->/!b; /<!--START_TAG-->/!d; r temp_gist_content.txt' README.md
 
 # Clean up the temporary file
 rm temp_gist_content.txt
