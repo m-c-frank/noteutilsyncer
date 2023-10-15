eb2041c3427e4eb993dffcacc3c1ba8f4c06eceb
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-15 18:48:36 +0200

    bugfixing

--- update_related_tools.sh
+++ update_related_tools.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-GIST_CONTENT=$(curl -s https://gist.githubusercontent.com/m-c-frank/5b7a099c0998e3030888125370b26195/raw)
+# Fetch the Gist content and save it to a temporary file
+curl -s https://gist.githubusercontent.com/m-c-frank/5b7a099c0998e3030888125370b26195/raw/ > temp_gist_content.txt
 
-# Use sed to replace the "Related Tools" section in README.md with the fetched Gist content
-sed -i '/^## Related Tools$/,/^##/c\'"$GIST_CONTENT" README.md
+# Use sed to replace the "Related Tools" section in README.md with the content from the temporary file
+sed -i '/^## Related Tools$/,/^##/{/^## Related Tools$/!{/^##/!d}; r temp_gist_content.txt' -e '}' README.md
+
+# Clean up the temporary file
+rm temp_gist_content.txt
