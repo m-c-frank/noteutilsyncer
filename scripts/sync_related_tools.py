486f3d124bfc471124f5f731e533dcbfc794dd0f
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-15 20:44:45 +0200

    putting secret handling stuff in workflow

--- scripts/sync_related_tools.py
+++ scripts/sync_related_tools.py
@@ -3,35 +3,26 @@ import subprocess
 import utils
 
 def main():
-    # Check and update the main repo's README
     if utils.check_and_update_readme():
-        # Commit and push changes to the main repo
-        subprocess.run(["git", "config", "--global", "user.name", "GitHub Action"])
-        subprocess.run(["git", "config", "--global", "user.email", "action@github.com"])
-        subprocess.run(["git", "add", "README.md"])
-        subprocess.run(["git", "commit", "-m", "Update Related Tools section from Gist"])
-        subprocess.run(["git", "push", "origin", "main", "--force", "--quiet", f"https://{os.environ['GH_PAT']}@github.com/m-c-frank/noteutilsyncer.git"])
-
-        # Fetch the updated list of repos from the gist
         repo_names = utils.split_gist_into_repos(utils.fetch_gist_content())
-
         for repo in repo_names:
+            if repo == "noteutilsyncer":
+                continue  # skip the current repository
+
             # Clone the repo
             subprocess.run(["git", "clone", f"https://github.com/m-c-frank/{repo}.git"])
             
-            # Change directory to the cloned repo
-            os.chdir(repo)
-
-            # Update the README
-            utils.check_and_update_readme()
-
-            # Commit and push changes
-            subprocess.run(["git", "add", "README.md"])
-            subprocess.run(["git", "commit", "-m", "Update Related Tools section from Gist"])
-            subprocess.run(["git", "push", "origin", "main", "--force", "--quiet", f"https://{os.environ['GH_PAT']}@github.com/m-c-frank/{repo}.git"])
-
-            # Change directory back to the main directory
-            os.chdir("..")
+            # Modify its README
+            with open(f"{repo}/README.md", 'r') as f:
+                content = f.read()
+            new_section = utils.create_related_tools_section(repo_names)
+            updated_content = utils.replace_section_in_content(content, new_section)
+            with open(f"{repo}/README.md", 'w') as f:
+                f.write(updated_content)
+            
+            # Commit the changes
+            subprocess.run(["git", "-C", repo, "add", "README.md"])
+            subprocess.run(["git", "-C", repo, "commit", "-m", "Update Related Tools section from noteutilsyncer"])
 
 if __name__ == "__main__":
     main()
