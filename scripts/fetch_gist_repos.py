5ed92b03b9a1abcbff48ebcbddd9368800be84a5
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-16 06:46:38 +0200

    added comments

--- scripts/fetch_gist_repos.py
+++ scripts/fetch_gist_repos.py
@@ -5,18 +5,17 @@ GIST_URL = "https://gist.githubusercontent.com/m-c-frank/5b7a099c0998e3030888125
 def fetch_gist_content():
     response = requests.get(GIST_URL)
     response.raise_for_status()
-    return response.text.strip().split('\n')
+    lines = response.text.strip().split('\n')
+    repos = []
+    for line in lines:
+        repo, description = line.split(",", 1)
+        repos.append((repo.strip("'"), description.strip("'")))
+    return repos
 
 def main():
-    repo_names = fetch_gist_content()
-    
-    # Excluding the current repository from the list
-    if "noteutilsyncer" in repo_names:
-        repo_names.remove("noteutilsyncer")
-
-    # Write the repo names to repos.txt
+    repos = fetch_gist_content()
     with open("repos.txt", "w") as f:
-        for repo in repo_names:
+        for repo, _ in repos:
             f.write(f"{repo}\n")
 
 if __name__ == "__main__":
--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -1,36 +1,25 @@
-import os
-
-def create_related_tools_section(repo_names):
-    formatted_links = "\n".join([f"- **[{name}](https://github.com/m-c-frank/{name})**" for name in repo_names])
-    section = f"\n{formatted_links}\n"
+def create_related_tools_section(repos):
+    formatted_tools = "\n".join([f"- **[{name}](https://github.com/m-c-frank/{name})** - {description}" for name, description in repos])
+    section = f"<!--START_TOKEN-->\n**Note Utilities Ecosystem**: A suite of tools designed to streamline and enhance your note-taking and information processing workflows.\n\n{formatted_tools}\n<!--END_TOKEN-->"
     return section
 
-def replace_token_with_links(repo_names):
-    readme_path = "README.md"
-    with open(readme_path, 'r') as file:
-        content = file.read()
+def update_readme(repos):
+    with open('README.md', 'r') as f:
+        content = f.read()
 
-    start_token = "<!--START_TOKEN-->"
-    end_token = "<!--END_TOKEN-->"
-    
-    start_index = content.find(start_token)
-    end_index = content.find(end_token)
+    new_section = create_related_tools_section(repos)
+    start_index = content.find("<!--START_TOKEN-->")
+    end_index = content.find("<!--END_TOKEN-->") + len("<!--END_TOKEN-->")
+    updated_content = content[:start_index] + new_section + content[end_index:]
 
-    # If tokens are not found, just return without making changes
-    if start_index == -1 or end_index == -1:
-        return
-
-    new_section = create_related_tools_section(repo_names)
-    updated_content = content[:start_index+len(start_token)] + new_section + content[end_index:]
-
-    with open(readme_path, 'w') as file:
-        file.write(updated_content)
+    with open('README.md', 'w') as f:
+        f.write(updated_content)
 
 def main():
     with open("../repos.txt", "r") as f:
-        repo_names = [line.strip() for line in f.readlines()]
+        repos = [(line.strip(), "") for line in f.readlines()]
 
-    replace_token_with_links(repo_names)
+    update_readme(repos)
 
 if __name__ == "__main__":
     main()
